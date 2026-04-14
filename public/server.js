d6dede3fa1b54e43186659e393399be3123ccb4f
Course User <cs307-user@winter2026-comp307-group13.cs.mcgill.ca>
2026-04-14 19:04:33 -0400

    formatting

--- public/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-const express = require("express");
-const mongoose = require("mongoose");
-const session = require("express-session");
-
-const path = require("path");
-
