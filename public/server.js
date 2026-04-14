39bcec9825ac112f008754b9135a6f045d29b883
Course User <cs307-user@winter2026-comp307-group13.cs.mcgill.ca>
2026-04-14 17:38:12 -0400

    added server.js

---
+++ public/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+const path = require("path");
+
