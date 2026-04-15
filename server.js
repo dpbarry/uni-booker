efa78147bb0ea2ddefc43e1376727bcc73adc35b
Course User <cs307-user@winter2026-comp307-group13.cs.mcgill.ca>
2026-04-15 16:05:29 -0400

    formatting

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("better-sqlite3");
 const session = require("express-session");
 
 const path = require("path");
